9917074b3f622320cccad42a38d0b4bcd61ab5b2
Victor Gabriel <victo091198@gmail.com>
2023-06-01 17:52:10 -0300

    Created jpa settings

--- src/main/java/com/victorgabriel/eventos/modules/customers/services/CreateCustomerService.java
+++ src/main/java/com/victorgabriel/eventos/modules/customers/services/CreateCustomerService.java
@@ -9,6 +9,9 @@ import java.util.List;
 @Service
 public class CreateCustomerService {
 
+    // A camada de services é aonde ficam as regras de negócios e validações
+    // Chamada para a camada de repository (banco de dados)
+
     private List<Customer> customers = new ArrayList<>();
     public Customer execute(Customer customer) {
         var existCustomer = this.customers.stream().filter(c -> c.getEmail().equals(customer.getEmail()));

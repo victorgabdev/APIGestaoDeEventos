9aaee0a919e7c487c8acd40fbff3035ea752c333
Victor Gabriel <victo091198@gmail.com>
2023-06-02 14:43:17 -0300

    Customers Entity Created

--- src/main/java/com/victorgabriel/eventos/modules/customers/entities/Customer.java
+++ src/main/java/com/victorgabriel/eventos/modules/customers/entities/Customer.java
@@ -3,22 +3,29 @@ package com.victorgabriel.eventos.modules.customers.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data  // vai colocar getter and setter em todos os meus atributos
 @AllArgsConstructor  // Criar um construtor com todos os atributos
 @NoArgsConstructor  // Criar um construtor vazio
+@Entity  // Bean para o Spring e para o jpa
+@Table(name = "customers")  // Na tabela do databse esse será o name
 public class Customer {
 
+    @Id  // Esclarecendo a minha primary key
+    @GeneratedValue(generator = "UUID")
     private UUID id;
+
     private String name;
     private String email;
     private String password;
     private String document;
     private String phone;
 
-    public UUID getId() {
-        return UUID.randomUUID();
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }

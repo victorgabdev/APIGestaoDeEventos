df4916e27cbd539dc67b4cd59096615449043ee6
Victor Gabriel <victo091198@gmail.com>
2023-06-03 11:18:23 -0300

    Created entity event

---
+++ src/main/java/com/victorgabriel/eventos/modules/events/entities/Events.java
@@ -0,0 +1,34 @@
+package com.victorgabriel.eventos.modules.events.entities;
+
+import com.victorgabriel.eventos.modules.organizers.entities.Organizer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "events")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Events {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String address;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+    private int availableQuantity;
+    private LocalDateTime createdAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "organizer_id")
+    private Organizer organizer;
+}
